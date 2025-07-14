c5ba90a085d129a249c8f904ee7998e359eec168
kartik11m <kartikmaheshwari698@gmail.com>
2025-07-15 02:23:22 +0530

    ride backend was created with fare

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
 const captainRoutes = require('./routes/captain.routes');
 const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDb();
 
@@ -29,5 +30,6 @@ app.get('/',(req,res) =>{
 app.use('/users' , userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps' , mapsRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
---
+++ Backend/services/ride.service.js
@@ -0,0 +1,64 @@
+const rideModel = require('../models/ride.model');
+const mapService = require('./maps.service');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+
+async function getFare(pickup, destination) {
+    if (!pickup || !destination) {
+        throw new Error('Pickup and destination are required');
+    }
+
+    const distanceTime = await mapService.getDistanceTime(pickup, destination);
+    // distanceTime: { distance, duration, distanceValue, durationValue }
+    // Convert meters to km, seconds to min
+    const distance = distanceTime.distanceValue / 1000; // km
+    const duration = distanceTime.durationValue / 60;   // min
+
+    // Example fare calculation logic
+    const baseFare = {
+        car: 50,
+        auto: 30,
+        bike: 20
+    };
+    const perKmRate = {
+        car: 15,
+        auto: 10,
+        bike: 7
+    };
+    const perMinRate = {
+        car: 2,
+        auto: 1.5,
+        bike: 1
+    };
+
+    return {
+        car: baseFare.car + (perKmRate.car * distance) + (perMinRate.car * duration),
+        auto: baseFare.auto + (perKmRate.auto * distance) + (perMinRate.auto * duration),
+        bike: baseFare.bike + (perKmRate.bike * distance) + (perMinRate.bike * duration),
+        distanceValue: distanceTime.distanceValue / 1000,
+        durationValue: distanceTime.durationValue / 60
+    };
+}
+
+
+module.exports.createRide = async ({ user, pickup, destination, vehicleType }) => {
+    if (!user || !pickup || !destination || !vehicleType) {
+        throw new Error('All fields are required');
+    }
+
+    const fareData = await getFare(pickup, destination);
+
+    // console.log(fareData);
+
+    const ride = await rideModel.create({
+        user,
+        pickup,
+        destination,
+        fare: fareData[vehicleType],
+        distance: fareData.distanceValue, // in meters
+        duration: fareData.durationValue  // in seconds
+    });
+
+    return ride;
+}
+
